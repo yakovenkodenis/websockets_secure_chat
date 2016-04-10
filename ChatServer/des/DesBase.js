8267081fe7ca0f675404759b58bddf0ea3187219
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2016-04-10 23:29:42 +0300

    Add new DES js implementation, needs to be refactored

--- ChatServer/des/DesBase.js
+++ ChatServer/des/DesBase.js
@@ -94,4 +94,122 @@ export default class DesBase {
         IV = this._guardAgainstUnicode(IV);
         this._iv = IV;
     }
+
+    _padData(data, pad, padmode) {
+        // Pad data depending on the mode
+        if (!padmode) {
+            // Get the default padding mode.
+            padmode = this.padMode;
+        }
+
+        if (pad && padmode === PAD_PKCS5) {
+            throw new Error('Cannot use a pad character with PAD_PKCS5');
+        }
+
+        if (padmode === PAD_NORMAL) {
+            if (data.length % this.block_size === 0) {
+                // No padding required
+                return data;
+            }
+
+            if (!pad) {
+                pad = this.padding;
+            }
+
+            if (!pad) {
+                throw new Error(
+                    'Data must be a multiple of ' +
+                    `${this.block_size} bytes in length. ` +
+                    'Use padmode=PAD_PKCS5 or set the pad character'
+                );
+            }
+
+            // data += (this.block_size - data.length % this.block_size) * pad;
+            data.push(
+                ...this._populateNumberToArray(
+                    this.block_size - data.length % this.block_size,
+                    this._stringToBytesArray(pad))
+            );
+        } else if (padmode === PAD_PKCS5) {
+            const pad_len = 8 - (data.length % this.block_size);
+            data.push(
+                ...this._populateNumberToArray(pad_len)
+            );
+        }
+
+        return data;
+    }
+
+    _unpadData(data, pad, padmode) {
+        // Unpad data depending on mode
+        if (!data) {
+            return data;
+        }
+
+        if (pad && padmode === PAD_PKCS5) {
+            throw new Error('Cannot use a pad character with PAD_PKCS5');
+        }
+
+        if (!padmode) {
+            // Get the default padding mode.
+            padmode = this.padMode;
+        }
+
+        if (padmode === PAD_NORMAL) {
+            if (!pad) {
+                // Get the default padding.
+                pad = this.padding;
+            }
+
+            if (pad) {
+                data = data.slice(0, -this.block_size);
+                data.push(
+                    ...this._rstrip(data[data.length - this.block_size].join(''))
+                        .split('');
+                );
+            }
+        } else if (padmode === PAD_PKCS5) {
+            const pad_len = data[data.length - 1];
+            data = data.slice(0, -pad_len);
+        }
+
+        return data;
+    }
+
+    _guardAgainstUnicode(data) {
+        if (data instanceof str) {
+            // TODO
+        }
+    }
+
+    _populateNumberToArray(number, pad=number) {
+        let array = [];
+        for (let i = 0; i < number; ++i) {
+            array.push(pad);
+        }
+        return array;
+    }
+
+    _stringToBytesArray(str) {
+        const buff = new Buffer(str, 'utf16le');
+        let resBuffer = [];
+
+        for (let i = 0; i < buff.length; ++i) {
+            resBuffer.push(buff[i]);
+        }
+
+        return resBuffer;
+    }
+
+    _rstrip(str, ch) {
+        let i = str.length - 1,
+            toSlice = 0;
+
+        while (i >= 0 && str[i] === ch) {
+            toSlice++;
+            i--;
+        }
+
+        return str.slice(0, -toSlice);
+    }
 }
