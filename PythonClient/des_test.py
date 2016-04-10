5e946bdb37cd66957d14f46dcb21c3bb6958de7e
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2016-04-10 23:29:56 +0300

    Add new DES py implementation. Needs to be refactored

--- PythonClient/des_test.py
+++ PythonClient/des_test.py
@@ -1,12 +1,16 @@
-from des import *
+from des_var2 import *
 
 
-data = b'Some random message'
+data = b'message'
 
-key = DES(b'PASSWORD', CBC, b'\0\0\0\0\0\0\0\0', pad=None, padmode=PAD_PKCS5)
+key = des(b'PASSWORD', mode=ECB, IV='\0\0\0\0\0\0\0\0', pad='*', padmode=PAD_NORMAL)
+# key = DES(b'PASSWORD', mode=ECB, pad=None, padmode=PAD_PKCS5)
 print(key)
 d = key.encrypt(data)
 print(d)
 print(key.decrypt(d))
 
-assert key.decrypt(d, padmode=PAD_PKCS5) == data
+for i in d:
+    print(hex(i))
+
+assert key.decrypt(d, padmode=PAD_NORMAL) == data
