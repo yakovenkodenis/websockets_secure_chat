d5c9a6bfcbb9ef3955204dfffc7208570cfbca84
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2016-04-14 15:46:59 +0300

    Update the setup.py description

--- PythonClient/setup.py
+++ PythonClient/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='PythonClient',
       version='1.0',
-      description='Python Distribution Utilities',
+      description='The Python client app for the WebSockets secure chat.',
       author='Denis Yakovenko',
       author_email='yakovenko.denis.a@gmail.com',
       url='https://www.github.com/yakovenkodenis/websockets_secure_chat',
