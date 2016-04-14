015291b5d894d0d7a74725bf0846ad88e5c85522
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2016-04-14 15:41:56 +0300

    Add a setup file

---
+++ PythonClient/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='PythonClient',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Denis Yakovenko',
+      author_email='yakovenko.denis.a@gmail.com',
+      url='https://www.github.com/yakovenkodenis/websockets_secure_chat',
+      packages=['diffie_hellman', 'hashes'])
