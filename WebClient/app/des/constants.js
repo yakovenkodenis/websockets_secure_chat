0be295942ef4c9eda334968fe22803d48cf59ae1
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2016-04-18 14:04:07 +0300

    Add encryption algorithms

---
+++ WebClient/app/des/constants.js
@@ -0,0 +1,7 @@
+// Modes of crypting / ciphering
+export const ECB = 0;
+export const CBC = 1;
+
+// Modes of padding
+export const PAD_NORMAL = 1
+export const PAD_PKCS5 = 2
---
+++ WebClient/app/des/des_test.js
@@ -0,0 +1,17 @@
+import {
+    des,
+    stringToHex,
+    hexToString
+} from './des_var2';
+
+
+var key = "PASSWORD";
+var message = "message";
+var ciphertext = des(key, message, 1, 0, '0x0000000000000000', '*');
+console.log("DES Test: " + stringToHex(ciphertext));
+console.log("DES MSG: " + des(key, ciphertext, 0, 0));
+
+let test_str = '0xf6dc54077b15dbaf';
+console.log(hexToString(test_str));
+console.log(ciphertext);
+console.log(des(key, hexToString(test_str), 0, 0));
