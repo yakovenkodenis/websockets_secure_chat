c35bdb0f8ff255d07f451182ab7fa053d0f78c2b
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2016-04-24 22:34:47 +0300

    Add plot of collission probabilities

--- PythonClient/hashes/collision.py
+++ PythonClient/hashes/collision.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from random import choice, seed
 from hash_function import hash_function
 
@@ -19,3 +21,17 @@ for j in range(5000):
             continue
         print(h, found[h], s)
     found[h] = s
+
+
+# Calculate the probability of collision for 100 keys using Birthday Attack
+n = num_of_all_hashes = 2 ** 8  # 256
+keys = 256
+no_coll_probs = np.array([(n - k) / n for k in range(keys)])
+keys_arr = np.array(range(keys))
+no_collision = np.prod(no_coll_probs)
+print(no_collision)
+coll_prob = 1 - no_collision
+print('Probability of collision in %d keys is %f' % (keys, coll_prob))
+
+plt.plot(keys_arr, 1 - no_coll_probs)
+plt.show()
