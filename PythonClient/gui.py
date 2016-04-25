8519579d5b4dc4a0a82ae368fbc3e518eb2c750d
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2016-04-25 23:46:35 +0300

    Set up chat communication between clients

--- PythonClient/gui.py
+++ PythonClient/gui.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import threading
 from tkinter import *
+from socketIO_client import SocketIO, LoggingNamespace
 
 
 tk = Tk()
@@ -21,18 +23,46 @@ nick.pack(side='bottom', fill='x', expand='true')
 log.pack(side='top', fill='both', expand='true')
 
 
-def loopproc():
-    print('Hello ' + name.get() + '!')
-    tk.after(1000, loopproc)
+ADDRESS = 'http://192.168.1.101'
+PORT = 8080
+
+
+def emit_message(address=ADDRESS, port=PORT, message=''):
+    with SocketIO(address, port, LoggingNamespace) as socketIO:
+        socketIO.emit('chat_message', {'payload': message, 'sender': 'me'},
+                      on_bbb_response)
+
+
+def listen_for_messages_loop():
+    with SocketIO(ADDRESS, PORT, LoggingNamespace) as socketIO:
+        socketIO.on('chat_message', add_message_to_chat_log)
+        socketIO.wait()
+
+
+def add_message_to_chat_log(*data):
+    if not data[0].get('message').get('sender'):
+        log.insert(END, data[0].get('message').get('payload') + '\n')
+        log.see('end')
 
 
 def sendproc(event):
     log.insert(END, name.get() + ': ' + text.get() + '\n')
+    log.see('end')
+
+    emit_message(message=text.get())
+
     text.set('')
 
 
-msg.bind('<Return>', sendproc)
+def on_bbb_response(*args):
+    print('on_bbb_response', args)
 
-tk.after(1000, loopproc)
+
+def on_chat_message_response(*args):
+    print('on_chat_message_response: ', args)
+
+
+msg.bind('<Return>', sendproc)
+threading.Thread(target=listen_for_messages_loop).start()
 
 tk.mainloop()
--- PythonClient/index.py
+++ PythonClient/index.py
@@ -8,12 +8,12 @@ def on_bbb_response(*args):
 
 
 def on_chat_message_response(*args):
-    print(args)
+    print('on_chat_message_response: ', args)
 
-with SocketIO('http://192.168.0.110', 8080, LoggingNamespace) as socketIO:
+
+with SocketIO('http://192.168.1.101', 8080, LoggingNamespace) as socketIO:
     socketIO.emit('hello', {'xxx': 'yyy'}, on_bbb_response)
-    # socketIO.wait_for_callbacks(seconds=1)
+    # socketIO.wait_for_callbacks(seconds=5)
 
     socketIO.on('chat_message', on_chat_message_response)
-
-print('hello')
+    socketIO.wait()
