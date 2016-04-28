f6cfc2a85ecaeba31407592d819a73766b36143f
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2016-04-29 01:49:52 +0300

    Implement DES subkeys generation and start implementing feistel function rounds

--- WebClient/app/des/des_test.js
+++ WebClient/app/des/des_test.js
@@ -1,17 +1,30 @@
-import {
-    des,
-    stringToHex,
-    hexToString
-} from './des_var2';
+// import {
+//     des,
+//     stringToHex,
+//     hexToString
+// } from './des_var2';
 
 
-var key = "PASSWORD";
-var message = "message";
-var ciphertext = des(key, message, 1, 0, '0x0000000000000000', 1);
-console.log("DES Test: " + stringToHex(ciphertext));
-console.log("DES MSG: " + des(key, ciphertext, 0, 0));
+// var key = "PASSWORD";
+// var message = "message";
+// var ciphertext = des(key, message, 1, 0, '0x0000000000000000', 1);
+// console.log("DES Test: " + stringToHex(ciphertext));
+// console.log("DES MSG: " + des(key, ciphertext, 0, 0));
 
-let test_str = '0xf6dc54077b15dbaf';
-console.log(hexToString(test_str));
-console.log(ciphertext);
-console.log(des(key, hexToString(test_str), 0, 0));
+// let test_str = '0xf6dc54077b15dbaf';
+// console.log(hexToString(test_str));
+// console.log(ciphertext);
+// console.log(des(key, hexToString(test_str), 0, 0));
+
+import DES from './DES';
+
+
+let d = new DES('qwertyui');
+
+
+let keyBits = d._stringToBitsArray('qwertyui');
+
+// let subkeys = d.getSubkeys(keyBits);
+// console.log(subkeys);
+
+// console.log(d._stringToBitsArray('hi'));
